d18ed096621729c9ee0ad2c707229c32a962dd48
Stephen Rothwell <sfr@canb.auug.org.au>
2009-10-12 15:56:17 +1100

    sound: use semicolons to end statements
    commit 0f48327eac5f65ad029d7112cac97577766730ba upstream.
    
    Fixes:
    
    sound/pci/hda/patch_via.c: In function 'patch_vt1718S':
    sound/pci/hda/patch_via.c:4951: error: expected expression before 'return'
    sound/pci/hda/patch_via.c: In function 'patch_vt1716S':
    sound/pci/hda/patch_via.c:5441: error: expected expression before 'return'
    sound/pci/hda/patch_via.c: In function 'patch_vt2002P':
    sound/pci/hda/patch_via.c:5794: error: expected expression before 'return'
    sound/pci/hda/patch_via.c: In function 'patch_vt1812':
    sound/pci/hda/patch_via.c:6148: error: expected expression before 'return'
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/patch_via.c
+++ sound/pci/hda/patch_via.c
@@ -4975,7 +4975,7 @@ static int patch_vt1718S(struct hda_codec *codec)
 	codec->patch_ops = via_patch_ops;
 
 	codec->patch_ops.init = via_auto_init;
-	codec->patch_ops.unsol_event = via_unsol_event,
+	codec->patch_ops.unsol_event = via_unsol_event;
 
 #ifdef CONFIG_SND_HDA_POWER_SAVE
 	spec->loopback.amplist = vt1718S_loopbacks;
@@ -5465,7 +5465,7 @@ static int patch_vt1716S(struct hda_codec *codec)
 	codec->patch_ops = via_patch_ops;
 
 	codec->patch_ops.init = via_auto_init;
-	codec->patch_ops.unsol_event = via_unsol_event,
+	codec->patch_ops.unsol_event = via_unsol_event;
 
 #ifdef CONFIG_SND_HDA_POWER_SAVE
 	spec->loopback.amplist = vt1716S_loopbacks;
@@ -5818,7 +5818,7 @@ static int patch_vt2002P(struct hda_codec *codec)
 	codec->patch_ops = via_patch_ops;
 
 	codec->patch_ops.init = via_auto_init;
-	codec->patch_ops.unsol_event = via_unsol_event,
+	codec->patch_ops.unsol_event = via_unsol_event;
 
 #ifdef CONFIG_SND_HDA_POWER_SAVE
 	spec->loopback.amplist = vt2002P_loopbacks;
@@ -6172,7 +6172,7 @@ static int patch_vt1812(struct hda_codec *codec)
 	codec->patch_ops = via_patch_ops;
 
 	codec->patch_ops.init = via_auto_init;
-	codec->patch_ops.unsol_event = via_unsol_event,
+	codec->patch_ops.unsol_event = via_unsol_event;
 
 #ifdef CONFIG_SND_HDA_POWER_SAVE
 	spec->loopback.amplist = vt1812_loopbacks;
