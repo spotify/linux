cdc53748f016ea0a01afe6f92f7635a0d56525f8
Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
2010-07-21 22:51:39 -0700

    xenfs: enable for HVM domains too
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
    (cherry picked from commit 43df95c44e71d009b5a73f104ff183f73af9526f)

--- drivers/xen/xenfs/super.c
+++ drivers/xen/xenfs/super.c
@@ -63,7 +63,7 @@ static struct file_system_type xenfs_type = {
 
 static int __init xenfs_init(void)
 {
-	if (xen_pv_domain())
+	if (xen_domain())
 		return register_filesystem(&xenfs_type);
 
 	printk(KERN_INFO "XENFS: not registering filesystem on non-xen platform\n");
@@ -72,7 +72,7 @@ static int __init xenfs_init(void)
 
 static void __exit xenfs_exit(void)
 {
-	if (xen_pv_domain())
+	if (xen_domain())
 		unregister_filesystem(&xenfs_type);
 }
 
