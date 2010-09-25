666755085c5f1c4c1fa6772d9f7285749af32b65
Ben Hutchings <ben@decadent.org.uk>
2010-09-25 18:36:44 +0100

    sched: Avoid ABI change due to rq change
    This structure is private but genksyms doesn't know that.

--- kernel/sched.c
+++ kernel/sched.c
@@ -542,6 +542,9 @@ struct rq {
 	struct load_weight load;
 	unsigned long nr_load_updates;
 	u64 nr_switches;
+#ifdef __GENKSYMS__
+	u64 nr_migrations_in;
+#endif
 
 	struct cfs_rq cfs;
 	struct rt_rq rt;
