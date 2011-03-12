linux-rockchip/linux-rockchip

vt6656: Use request_firmware() to load firmware
The file added to linux-firmware is a copy of the current array which
does not have a recognisable header, so no validation is done.

Change the firmware version check to accept newer versions.

Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

drivers/staging/vt6656/main_usb.c
@@ -1272,6 +1272,9 @@ static void __devexit vt6656_disconnect(struct usb_interface *intf)
 
 	device_release_WPADEV(device);
 
+	if (device->firmware)
+		release_firmware(device->firmware);
+
 	usb_set_intfdata(intf, NULL);
 	usb_put_dev(interface_to_usbdev(intf));
 

spotify/linux

commit 31d5bbf3da8c3c9de5944f2c09cbc7ea5a72bdb4 upstream.
[bwh: Restore firmware.c without the firmware blob; adjust for 2.6.32]

drivers/staging/vt6656/main_usb.c
@@ -1315,6 +1315,9 @@ static void vntwusb_disconnect(struct usb_interface *intf)
 //2008-0714-01<Add>by MikeLiu
 device_release_WPADEV(pDevice);
 
+	if (pDevice->firmware)
+		release_firmware(pDevice->firmware);
+
 	usb_set_intfdata(intf, NULL);
 //2008-0922-01<Add>by MikeLiu, decrease usb counter.
      usb_put_dev(interface_to_usbdev(intf));