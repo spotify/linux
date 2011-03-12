139062cff1392571e05f5f714182303009e78149
Debian kernel team <debian-kernel@lists.debian.org>
2011-03-12 21:01:42 +0000

    Revert "inotify-send-IN_UNMOUNT-events.patch"

--- fs/notify/inotify/inotify_user.c
+++ fs/notify/inotify/inotify_user.c
@@ -106,11 +106,8 @@ static inline __u32 inotify_arg_to_mask(u32 arg)
 {
 	__u32 mask;
 
-	/*
-	 * everything should accept their own ignored, cares about children,
-	 * and should receive events when the inode is unmounted
-	 */
-	mask = (FS_IN_IGNORED | FS_EVENT_ON_CHILD | FS_UNMOUNT);
+	/* everything should accept their own ignored and cares about children */
+	mask = (FS_IN_IGNORED | FS_EVENT_ON_CHILD);
 
 	/* mask off the flags used to open the fd */
 	mask |= (arg & (IN_ALL_EVENTS | IN_ONESHOT));
