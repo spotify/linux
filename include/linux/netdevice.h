0b7c52d4544e9302124f85f2c6c541dc6ca7a80c
Pierre Carrier <pierre@spotify.com>
2012-02-01 01:36:47 +0100

    netdev-and-vpd-backports-needed-by-bnx2

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -227,6 +227,11 @@ struct netdev_hw_addr_list {
 	int			count;
 };
 
+#define netdev_uc_count(dev) ((dev)->uc.count)
+#define netdev_uc_empty(dev) ((dev)->uc.count == 0)
+#define netdev_for_each_uc_addr(ha, dev) \
+	list_for_each_entry(ha, &dev->uc.list, list)
+
 #define netdev_mc_count(dev) ((dev)->mc_count)
 #define netdev_mc_empty(dev) (netdev_mc_count(dev) == 0)
 
