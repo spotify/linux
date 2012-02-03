864e2c4089430edf048d8a04b1b877244b8c7a10
Debian kernel team <debian-kernel@lists.debian.org>
2012-02-03 03:09:40 +0000

    Revert "splice-direct_splice_actor-should-not-use-pos-in-sd.patch"

--- fs/splice.c
+++ fs/splice.c
@@ -1223,8 +1223,7 @@ static int direct_splice_actor(struct pipe_inode_info *pipe,
 {
 	struct file *file = sd->u.file;
 
-	return do_splice_from(pipe, file, &file->f_pos, sd->total_len,
-			      sd->flags);
+	return do_splice_from(pipe, file, &sd->pos, sd->total_len, sd->flags);
 }
 
 /**
